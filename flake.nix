7714756a6a00e4ff88e5859ae869f41527758a3b
Jermeiah S <owner@arouzing.xyz>
2023-09-29 18:43:42 -0400

    init commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+        nixos-generators = {
+            url = "github:nix-community/nixos-generators";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+    outputs = inputs:
+    with inputs;
+    let
+        # supportedSystems = ["x86_64-linux" "x86-linux"];
+        defaultSystem = "x86_64-linux";
+        specialArgs = {inherit self inputs;};
+        # forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+        sharedModules = [
+            nixos-generators.nixosModules.all-formats
+        ];
+        mkNixos = system: systemModules: config: nixpkgs.lib.nixosSystem {
+            inherit system specialArgs;
+            modules = sharedModules ++ systemModules ++ [
+                { system.stateVersion = "23.05";
+                }
+                config
+            ];
+        };
+    in {
+        nixosConfigurations = {
+            test = mkNixos defaultSystem [nixos-generators.nixosModules.linode] ./configuration.nix;
+        };
+        packages.x86_64-linux = {
+            linode = self.nixosConfigurations.test.config.formats.linode;
+        };
+    };
+}
