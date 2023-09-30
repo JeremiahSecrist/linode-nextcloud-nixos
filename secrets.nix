ac52cc3442295f5abca586b9dc3c1cbb9ba143de
Jermeiah S <owner@arouzing.xyz>
2023-09-30 16:01:12 -0400

    added remote keys

--- secrets.nix
+++ secrets.nix
@@ -1,7 +1,7 @@
 let
-  #   machines = {
-  #      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINmveI8P/aqICzkZ+2nTxbV7RB5/ZEsgsxZmC3IajYxL root@ubuntu-s-1vcpu-1gb-nyc3-01"; # A normal SSH ed25519 key.
-  #   };
+  machines = {
+    linode = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExj3H8k2iLriwR/Ui9RzIaox+qmy8H1kvKZjNAn9ddZ"; # A normal SSH ed25519 key.
+  };
   pth = "secrets";
   users = {
     sky = "age1yubikey1qd07mh0sznx06zvy3qrzfyajeaveerdg4awzp4t4s5dvc7285n4a23hk6j2"; # the public key
@@ -9,5 +9,6 @@ let
 in {
   "${pth}/nextcloudPassword".publicKeys = [
     users.sky
+    machines.linode
   ];
 }
