8bb8718d10d9f177f3bf6aa4b175741460923061
Jermeiah S <owner@arouzing.xyz>
2024-01-18 21:34:55 -0500

    sda

--- hosts/nextcloud/hardware.nix
+++ hosts/nextcloud/hardware.nix
@@ -9,7 +9,7 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
-  boot.loader.grub.devices = ["/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f"];
+  boot.loader.grub.devices = ["/dev/sda"];
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f";
       fsType = "ext4";
